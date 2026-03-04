a43d987a663b3686216cbc3737f60c84564d6db4
devcolony <userscompiled@gmail.com>
2026-03-04 07:30:52 -0600

    add local dev server

---
+++ serve.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3503;
+const MIME = {
+  ".html": "text/html",
+  ".js": "application/javascript",
+  ".css": "text/css",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".ico": "image/x-icon",
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url === "/" ? "/index.html" : req.url;
+  filePath = path.join(__dirname, filePath);
+  const ext = path.extname(filePath);
+  const contentType = MIME[ext] || "application/octet-stream";
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Not found");
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("Hatchery running at http://localhost:" + PORT);
+});
